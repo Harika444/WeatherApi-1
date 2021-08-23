9a0babcd76c2002ce8c0613b686ab8ab581d4c11
Venkata Nagendra Kumar Kone <45361985+knagu@users.noreply.github.com>
2021-08-23 23:53:57 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -162,6 +162,9 @@ resource "kubernetes_ingress" "weather_api_ingress" {
       "kubernetes.io/ingress.class" = "alb"
       "alb.ingress.kubernetes.io/scheme" = "internet-facing"
       "alb.ingress.kubernetes.io/target-type" = "ip"
+      "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:252540742691:certificate/5ea332d3-9e4c-4a82-8149-b30cd3aaf145"
+      "alb.ingress.kubernetes.io/listen-ports": "[{"HTTP": 80}, {"HTTPS":443}]"
+      "alb.ingress.kubernetes.io/actions.ssl-redirect": "{"Type": "redirect", "RedirectConfig": { "Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}"
     }
   }
   spec {

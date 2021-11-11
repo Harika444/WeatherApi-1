256fc67cf58402cdb46d082f7b888319dcf40d68
Harika444 <47811739+Harika444@users.noreply.github.com>
2021-11-11 20:11:02 +0530

    Update deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -194,18 +194,3 @@ resource "kubernetes_ingress" "weather_api_ingress" {
 }
 
 
-resource "kubernetes_persistent_volume_claim" "pvc" {
-  metadata {
-    name            = "terraform-example"
-  }
-  spec {
-    storage_class_name = "gp2"
-    access_modes = ["ReadWriteMany"]
-    resources {
-      requests = {
-        storage = "1Gi"
-      }
-    }
-    volume_name     = "terraform-example"
-  }
-}
